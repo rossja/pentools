945b02b9b0d0c6ca6a614808658f6b3a0d3a410a
Jason Ross <algorythm@gmail.com>
2014-06-03 15:13:37 -0400

    updated HIGS

--- host_review/win/HIGS.bat
+++ host_review/win/HIGS.bat
@@ -3,8 +3,8 @@ REM ---------------------------------------------
 REM Host Information Gathering Script (Windows Edition)
 REM Written by Jason Ross <algorythm@gmail.com>
 REM ---------------------------------------------
-REM Version: 0.2
-REM Last Modified: 2013.10.24
+REM Version: 0.3
+REM Last Modified: 2014.06.03
 REM ---------------------------------------------
 REM Simple Windows batch file to gather system
 REM information for use during a host config
@@ -15,15 +15,22 @@ REM ---------------------------------------------
 REM Depends on the psloglist tool from SysInternals
 REM (now Microsoft). It's part of the PSTools suite
 REM http://technet.microsoft.com/en-us/sysinternals/bb896649.aspx
+REM I've included it in this repo for convenience
 REM ---------------------------------------------
 REM Tested on the following platforms:
 REM Server:
 REM    Windows 2003 SP2
+REM    Windows Server 2012
 REM Desktop:
 REM    Windows 7 Professional (32 bit)
 REM    Windows 7 Professional (64 bit)
 REM    Windows XP SP3
 REM    Windows XP SP2
+REM ---------------------------------------------
+REM TODO: update this to check for powershell
+REM       and use that to perform more robust
+REM       checks. Also needs to be updated badly
+REM       to utilize the newer features of IIS
 REM ---------------------------------------------
 REM This program is free software: you can redistribute it and/or modify
 REM it under the terms of the GNU General Public License as published by
@@ -38,13 +45,15 @@ REM
 REM You should have received a copy of the GNU General Public License
 REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
 REM ---------------------------------------------
-REM Copyright 2010 Jason Ross <algorythm /at/ gmail /dot/ com>
+REM Copyright 2010-2014 Jason Ross <algorythm /at/ gmail /dot/ com>
 REM ---------------------------------------------
 md %COMPUTERNAME%
 cd %COMPUTERNAME%
 
+
+echo =================
 echo Gathering basic information
-echo ================= > basic-info.txt
+echo =================
 echo Host Configuration Tool >> basic-info.txt
 echo Output Created on %DATE% at %TIME% >> basic-info.txt
 echo ================= >> basic-info.txt
@@ -54,101 +63,103 @@ echo This script running as: %USERNAME% >> basic-info.txt
 echo ================= >> basic-info.txt
 echo Environment Variables: >> basic-info.txt
 set >> basic-info.txt
-echo ================= >> basic-info.txt
 echo Done!
 echo.
 
+
+echo =================
 echo Detecting installed software
-echo ================= > software.txt
+echo =================
 reg export HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall reg.txt
-
 find "DisplayName" reg.txt |find /V "ParentDisplayName" > tmplist.txt
-
 for /f "tokens=2,3 delims==" %%a in (tmplist.txt) do (echo %%a >> software.txt)
-
-echo ================= >> software.txt
 echo Done!
 echo.
 
+
+echo =================
+echo Detecting installed hotfixes
+echo =================
+wmic qfe list >hotfixes.txt
+echo Done!
+echo.
+
+echo =================
 echo Creating list of registered services
-echo ================= > services.txt
-
+echo =================
 sc query state= all >> services.txt
-
-echo ================= >> services.txt
 echo Done!
 echo.
+
 
 echo Enumerating network servers visible to this host
 echo ================= > net-view.txt
 net view >> net-view.txt
-echo ================= >> net-view.txt
 echo Done!
 echo.
 
+
+echo =================
 echo Gathering local user and group information
-
-echo ================= > localgroups.txt
+echo =================
 net localgroup >> localgroups.txt
-echo ================= >> localgroups.txt
-echo ================= > localusers.txt
 net user >> localusers.txt
-echo ================= >> localusers.txt
 echo Done!
 echo.
 
 
+echo =================
 echo Gathering shared folder information
-
-echo ================= > shares.txt
+echo =================
 net view \\%COMPUTERNAME% >> shares.txt
-
-echo ================= >> shares.txt
 echo Done!
 echo.
 
+
+echo =================
 echo Gathering IP configuration
-
-echo ================= > ipconfig.txt
+echo =================
 ipconfig /all >> ipconfig.txt
-
-echo ================= >> ipconfig.txt
 echo Done!
 echo.
 
+
+echo =================
 echo Detecting open ports
-
-echo ================= > netstat.txt
+echo =================
 netstat -ban >> netstat.txt
-echo ================= >> netstat.txt
 echo Done!
 echo.
 
-echo ================= > at.txt
+
+echo =================
 echo Checking scheduled jobs
-echo ================= >> at.txt
+echo =================
 at >> at.txt
 echo Done!
 echo.
 
 
 
-echo ================= > at.txt
+echo =================
 echo Checking installed updates
-echo ================= >> at.txt
+echo =================
 dism /online /get-packages /format:table /English >packages.txt
 echo Done!
 echo.
 
 
-echo Collecting logs, this may take a bit...
-..\psloglist -x system > system.log
-..\psloglist -x security > security.log
-..\psloglist -x application > application.log
+echo =================
+echo Checking IIS sites
+echo =================
+%windir%\system32\inetsrv\AppCmd.exe list site > IIS_sites.txt
 echo Done!
 echo.
 
+
+echo =================
 echo Dumping the registry
+echo =================
 reg export HKLM hklm.reg
 reg export HKCU hkcu.reg
 reg export HKCU hkcr.reg
@@ -157,14 +168,26 @@ reg export HKCU hkcc.reg
 echo Done!
 echo.
 
-echo Dumping passwords
-fgdump -l fgdump.log
+
+echo =================
+echo Collecting logs, this may take a bit...
+echo =================
+..\psloglist -x system > system.log
+..\psloglist -x security > security.log
+..\psloglist -x application > application.log
 echo Done!
 echo.
 
-echo Cleaning up temporary files
-del reg.txt tmplist.txt
 
+echo =================
+echo Cleaning up temporary files
+echo =================
+del reg.txt tmplist.txt
+echo Done!
 echo.
+
+
+echo =================
 echo Host Information Gathering Script finished.
+echo =================
 echo.
