886ed6769bd616c0f120e606cf99a2b9b8876abb
Jason Ross <algorythm@gmail.com>
2018-08-30 08:43:43 -0400

    added a version of HIGS that has zero user-friendliness for environments that are super averse to everything sensible

---
+++ host_review/MSWindows/HIGS-minimal.bat
@@ -0,0 +1,29 @@
+@echo off
+md %COMPUTERNAME%
+cd %COMPUTERNAME%
+echo Output Created on %DATE% at %TIME% >> basic-info.txt
+echo Domain: %USERDOMAIN >> basic-info.txt
+echo This host: %COMPUTERNAME% >> basic-info.txt
+echo This script running as: %USERNAME% >> basic-info.txt
+echo Environment Variables: >> basic-info.txt
+set >> basic-info.txt
+reg export HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall reg.txt
+find "DisplayName" reg.txt |find /V "ParentDisplayName" > tmplist.txt
+for /f "tokens=2,3 delims==" %%a in (tmplist.txt) do (echo %%a >> software.txt)
+wmic qfe list >hotfixes.txt
+sc query state= all >> services.txt
+net view >> net-view.txt
+net localgroup >> localgroups.txt
+net user >> localusers.txt
+net view \\%COMPUTERNAME% >> shares.txt
+ipconfig /all >> ipconfig.txt
+netstat -ban >> netstat.txt
+at >> at.txt
+dism /online /get-packages /format:table /English >packages.txt
+%windir%\system32\inetsrv\AppCmd.exe list site > IIS_sites.txt
+reg export HKLM hklm.reg
+reg export HKCU hkcu.reg
+reg export HKCR hkcr.reg
+reg export HKU hku.reg
+reg export HKCC hkcc.reg
+del reg.txt tmplist.txt
